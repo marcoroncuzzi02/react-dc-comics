524de2d418b98893335fe2e3b69b87e3e9c46195
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-02-24 17:12:21 +0100

    ADD footer

--- src/FooterPage.jsx
+++ src/FooterPage.jsx
@@ -1,7 +1,66 @@
 const FooterPage = () =>{
     return(
         <footer>
-            
+            <div className="background-dc up-footer-container">
+                <div className="first-footer-container">
+                    <div className="list-container">
+                        <h2>DC COMICS</h2>
+                        <ul>
+                            <li> <a href="">dc comics</a></li>
+                            <li> <a href="">dc comics</a></li>
+                            <li> <a href="">dc comics</a></li>
+                            <li> <a href="">dc comics</a></li>
+                            <li> <a href="">dc comics</a></li>
+                        </ul>
+                    </div>
+                    <div className="list-container">
+                        <h2>SHOP</h2>
+                        <ul>
+                            <li> <a href="">dc comics</a></li>
+                            <li> <a href="">dc shop</a></li>
+                            <li> <a href="">dc shop</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="list-container">
+                    <h2>DC</h2>
+                    <ul>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                    </ul>
+                </div>
+                <div className="list-container">
+                    <h2>SITES</h2>
+                    <ul>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                        <li> <a href="">dc terms</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="down-footer-container">
+              <button className="btn">sign-up now</button>
+              <div className="social">
+                <span>follow us</span>
+                <img src="\img\footer-facebook.png" alt="" />
+                <img src="\img\footer-periscope.png" alt="" />
+                <img src="\img\footer-pinterest.png" alt="" />
+                <img src="\img\footer-twitter.png" alt="" />
+                <img src="\img\footer-youtube.png" alt="" />
+              </div>
+            </div>
         </footer>
     )
 }
