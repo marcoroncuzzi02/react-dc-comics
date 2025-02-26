da5177022e1d2dd21c8fe57ea02fcbd16d7d355d
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-02-26 12:32:28 +0100

    ADD header props

--- src/HeaderPage.jsx
+++ src/HeaderPage.jsx
@@ -1,3 +1,17 @@
+const links= [
+    {id: 1, title:"COMICS"},
+    {id: 2, title:"MOVIES"},
+    {id: 3, title:"MOVIES"},
+    {id: 4, title:"TV"},
+    {id: 5, title:"GAMES"},
+    {id: 6, title:"COLLECTIBLES"},
+    {id: 7, title:"VIDEOS"},
+    {id: 8, title:"FANS"},
+    {id: 9, title:"NEWS"},
+    {id: 10, title:"SHOP"},
+
+]
+
 const HeaderPage = () =>{
     return(
         <header>
@@ -5,16 +19,14 @@ const HeaderPage = () =>{
                 <img src="img\dc-logo.png" alt="" />
             </figure>
             <ul>
-                <li><a className="active" href="">CHARACTER</a></li>
-                <li><a href="">COMICS</a></li>
-                <li><a href="">MOVIES</a></li>
-                <li><a href="">TV</a></li>
-                <li><a href="">GAMES</a></li>
-                <li><a href="">COLLECTIBLES</a></li>
-                <li><a href="">VIDEOS</a></li>
-                <li><a href="">FANS</a></li>
-                <li><a href="">NEWS</a></li>
-                <li><a href="">SHOP</a></li>
+                {links.map( (element) => {
+                    return(
+                        <li key="element.id">
+                            <a href="">{element.title}</a>
+                        </li>
+                    )
+                    })
+                }
             </ul>
         </header>
     )
