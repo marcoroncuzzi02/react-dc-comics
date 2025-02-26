8bccb6e5658909e42da06a60055e837458a6212a
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-02-26 16:45:06 +0100

    END

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -11,24 +11,24 @@ const MainPage = () =>{
                 <Card/>
             </div>
 
-            <div className="cards">
-                <div className="card">
+            <div className="nav-cards">
+                <div className="nav-card">
                     <img src="img\buy-comics-digital-comics.png" alt="" />
                     <span>DIGITAL COMICS</span>
                 </div>
-                <div className="card">
+                <div className="nav-card">
                     <img src="img\buy-comics-merchandise.png" alt="" />
                     <span>DC MERCHANDISE</span>
                 </div>
-                <div className="card">
+                <div className="nav-card">
                     <img src="img\buy-comics-subscriptions.png" alt="" />
                     <span>SUBSCRIPTION</span>
                 </div>
-                <div className="card">
+                <div className="nav-card">
                     <img src="img\buy-comics-shop-locator.png" alt="" />
                     <span>COMING SHOP LOCATOR</span>
                 </div>
-                <div className="card">
+                <div className="nav-card">
                     <img src="img\buy-dc-power-visa.svg" alt="" />
                     <span>DC POWER VISA</span>
                 </div>
